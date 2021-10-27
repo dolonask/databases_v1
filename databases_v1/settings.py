0db95c004d37ce8d1d6a5f8f5c824f1b3cd21df4
ntabyldieva <tabyldieva1296@gmail.com>
2021-10-27 16:44:21 +0600

    postgre

--- databases_v1/settings.py
+++ databases_v1/settings.py
@@ -124,21 +124,30 @@ WSGI_APPLICATION = 'databases_v1.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'database_crm_1',
+#         'USER': 'crm_admin',
+#         'PASSWORD': 'qwerTY123',
+#         'HOST': '127.0.0.1',
+#         'PORT': '5432',
+#     }
+# }
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'database_crm_1',
-        'USER': 'crm_admin',
-        'PASSWORD': 'qwerTY123',
+        'NAME': 'database_crm2',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
 }
-
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db-crm-4.sqlite3',
+#         'NAME': BASE_DIR / 'db-crm-1.sqlite3',
 #     }
 # }
 
